71be8d512f2a8586716827cff09acfab01f9e24a
arontseggai <aron.tseggai@gmail.com>
2017-10-30 10:01:44 +0100

    This adds slugs

--- lib/rumbl/shows/video.ex
+++ lib/rumbl/shows/video.ex
@@ -3,11 +3,12 @@ defmodule Rumbl.Shows.Video do
   import Ecto.Changeset
   alias Rumbl.Shows.Video
 
-
+  @primary_key {:id, Rumbl.Custom.Permalink, autogenerate: true}
   schema "videos" do
     field :description, :string
     field :title, :string
     field :url, :string
+    field :slug, :string
     belongs_to :user, Rumbl.Accounts.User
     belongs_to :category, Rumbl.Shows.Category
 
@@ -21,8 +22,28 @@ defmodule Rumbl.Shows.Video do
   def changeset(%Video{} = video, attrs) do
     video
     |> cast(attrs, @required_fields ++ @optional_fields)
+    |> slugify_title()
     |> assoc_constraint(:category, [message: "this category is not valid"])
-    # |> foreign_key_constraint(:category, [message: "this category is not valid"])
     |> validate_required(@required_fields)
+  end
+
+  defp slugify_title(changeset) do
+    if title = get_change(changeset, :title) do
+      put_change(changeset, :slug, slugify(title))
+    else
+      changeset
+    end
+  end
+
+  defp slugify(string) do
+    string
+      |> String.downcase()
+      |> String.replace(~r/[^\w-]+/u, "-")
+  end
+
+  defimpl Phoenix.Param, for: Rumbl.Shows.Video do
+    def to_param(%{slug: slug, id: id}) do
+      "#{id}-#{slug}"
+    end
   end
 end
