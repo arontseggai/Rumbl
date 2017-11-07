f1ffc76a5b0d99a007e96f0745c252c33dc41412
arontseggai <aron.tseggai@gmail.com>
2017-11-07 12:13:03 +0100

    Adds new homepage. wip

--- lib/rumbl_web/views/page_view.ex
+++ lib/rumbl_web/views/page_view.ex
@@ -1,3 +1,15 @@
 defmodule RumblWeb.PageView do
   use RumblWeb, :view
+
+  import RumblWeb.WatchView, only: [player_id: 1]
+
+
+  def truncate_description(description) do
+    cond do
+      String.length(description) > 130 ->
+        String.slice(description, 0..130) <> "..."
+      true ->
+        description
+    end
+  end
 end
